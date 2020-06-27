493cd3a0e40799257e373bb2f0025048bfdcfc36
Chris <mail@chrissewell.net>
2020-06-27 14:54:42 +0100

    seed aspweb project

---
+++ Pages/Maps.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace openra_rc_v3.Pages
+{
+    public class MapsModel : PageModel
+    {
+        private readonly ILogger<MapsModel> _logger;
+
+        public MapsModel(ILogger<MapsModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
